ad5841289c51d9997f517384e263b18df67244fa
skurka <skurka@ukaachen.de>
2021-08-27 14:15:42 +0200

    source and domain configurable

--- epix-batch-cli.sh
+++ epix-batch-cli.sh
@@ -2,8 +2,10 @@
 SEP=","
 BATCHSIZE=10
 EPIX_SERVICE="https://demo.ths-greifswald.de/epix/epixService"
+DOMAIN="Demo"
+SOURCE="dummy_safe_source"
 
-TEMP=`getopt -o hd:b:s: --long help,delimiter:,batch:,epix-service: -n "$(basename "$BASH_SOURCE")" -- "${@}"`
+TEMP=`getopt -o hd:b:s:n:e: --long help,delimiter:,batch:,epix-service:,domain:,source: -n "$(basename "$BASH_SOURCE")" -- "${@}"`
 
 if [ $? != 0 ] ; then exit 1 ; fi
 
@@ -25,6 +27,8 @@ OPTIONS
   -d --delimiter	Set field delimiter (default: ${SEP})
   -b --batch		Request Master-Patient-Index in batches of # datasets (default: ${BATCHSIZE})
   -s --epix-service	Set E-PIX service URL (default: ${EPIX_SERVICE})
+  -n --domain		Set E-PIX domain (default: ${DOMAIN})
+  -e --source		Set E-PIX source (default: ${SOURCE})
 
 EOF
         							shift 1; exit ;;
@@ -32,6 +36,8 @@ EOF
 		-d|--delimiter)		SEP="$2";		shift 2; continue ;;
 		-b|--batch)		BATCHSIZE="$2";		shift 2; continue ;;
 		-s|--epix-service)	EPIX_SERVICE="$2";	shift 2; continue ;;
+		-n|--domain)		DOMAIN="$2";		shift 2; continue ;;
+		-e|--source)		SOURCE="$2";		shift 2; continue ;;
 	esac
 
 	echo "ERROR: Unknown parameter ${1}"
@@ -41,8 +47,8 @@ done
 function send_request() {
 	REQE="$1"
 	DATA="$(echo -n "$2")"
-	REQH='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.epix.ttp.icmvc.emau.org/"><soapenv:Header/><soapenv:Body><ser:requestMPIBatch><mpiRequest><domainName>Demo</domainName>'
-	REQF='<sourceName>dummy_safe_source</sourceName></mpiRequest></ser:requestMPIBatch></soapenv:Body></soapenv:Envelope>'
+	REQH='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.epix.ttp.icmvc.emau.org/"><soapenv:Header/><soapenv:Body><ser:requestMPIBatch><mpiRequest><domainName>'"$DOMAIN"'</domainName>'
+	REQF='<sourceName>'"$SOURCE"'</sourceName></mpiRequest></ser:requestMPIBatch></soapenv:Body></soapenv:Envelope>'
 	ORDER="$(echo "$DATA" | awk -F"$SEP" '{print $0 FS NR}' | sort -t"$SEP" | rev | cut -d"$SEP" -f1 | rev)"
 	COUNT="$(echo "$DATA" | awk -F"$SEP" '{d[$0]++} END{for(l in d){print l FS d[l]}}' | sort -t"$SEP" | rev | cut -d"$SEP" -f1 | rev)"
 	echo "$(curl --silent -X POST -H "Content-Type: text/xml" --data-binary @<(echo "$REQH$REQE$REQF") "$EPIX_SERVICE" | xsltproc <(echo "$XSLT") - | sort -t"$SEP" | paste -d"$SEP" <(echo "$COUNT") - | awk -F',' '{for(i=1;i<=$1;i++){print}}' | cut -d"$SEP" -f1 --complement | paste -d"$SEP" <(echo "$ORDER") - | sort -nt"$SEP" | cut -d"$SEP" -f1 --complement | rev | cut -d"$SEP" -f1 | rev)"
